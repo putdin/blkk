c0f98b2cb8d31c804efbec35a942094ad2296414
Moh Saifuddin <moh541fuddin@gmail.com>
2023-12-22 20:58:59 +0700

    done

---
+++ application/modules/home/views/v_home/about.php
@@ -0,0 +1,26 @@
+<!-- ##### Regular Page Area Start ##### -->
+    <div class="regular-page-area section-padding-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-content">
+                        <h4>Profil BLKK-YPPRM</h4>
+                        <p>Balai Latihan Kerja Komunitas yang saya tempati merupakan bagian dari Yayasan Roudlotul Muta’abiddin yang merupakan Yayasan berwawasan kebangsaan, merangkul siswa dari semua golongan, suku, ras dan agama. Pendidikan budi pekerti, muatan local dan pendidikan kewirausahaan merupakan terobosan yang diutamakan disekolah ini. Tahun ajaran 2022/2023 merupakan tahun pertama lulusan dari sekolah ini bisa mengikuti pelatihan.
+                        Terdapat banyak fasilitas yang terdapat Balai Latihan Kerja Komunitas yang bisa dimanfaatkan untuk meningkatkan bakat dari para siswa.
+                        </p>
+                        <h4>Visi</h4>
+                        <p>Menjadikan pusat unggulan dalam pengembangan keterampilan dan peningkatan keberdayaan masyarakat melalui pelatihan yang berorientansi pada kebutuhan pasar kerja.</p>
+                        <h4>Misi</h4>
+                        <p>
+                        1.	Memberikan pelatihan berkualitas tinggi dan relevan untuk memenuhi kebutuhan tenaga kerja lokal  dan nasional.<br>
+                        2.	Membuka peluang pelatihan kepada masyarakat di semua lapisan.<br>
+                        3.	Membangun dan menjalin kemitraan yang kuat dengan pemerintah, lembaga pendidikan dan sektor bisnis untuk memastikan pelatihan yang terkini dan relevan.<br>
+                        4.	Mendorong dan mendukung pengembangan wirausaha di desa dengan memberikan pelatihan dalam kewirausahaan dan dukungan bisnis.<br>
+                        5.	Mengembangkan dan mengadopsi inovasi dalam metode pelatihan dan kurikulum untuk menjaga relevansi kami dalam menghadapi perubahan cepat di pasar kerja.<br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Regular Page Area End ##### -->
